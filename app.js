45c1659aba7051130077023fc76c52b3ce829633
yethuaungptu <yethuaung481995@gamil.com>
2018-11-11 00:53:41 +0630

    add session, modify template

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(session({
+  secret: 'T@Ch H0u$@ $TUD!0',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +26,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.locals.user = req.session.user;
+  next();
+});
 app.use('/', indexRouter);
+app.use(function (req, res, next) {
+  // res.locals.active = req.path;
+  console.log('user path', req.path);
+  if(req.session.user){
+    next();
+  }else{
+    // req.flash("warning", "Authorization failed! Please login");
+    // req.flash('forward', req.path);
+    res.redirect('/signin');
+  }
+
+});
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if(req.session.user){
+    res.render('index', { title: 'Express' });
+  }else {
+    res.render('common/signin')
+  }
+
 });
+router.get('/signin', function(req, res, next) {
+  res.render('common/signin', { title: 'Express' });
+});
+
+router.post('/signin',function (req, res, next) {
+  if(req.body.loginUsername == "TechHouse@admin" && req.body.loginPassword == "Tech@House!001"){
+    res.cookie('admin', req.body.loginUsername);
+    req.session.user = { name: req.body.loginUsername};
+    res.redirect('/')
+  }else {
+    res.render('common/signin');
+  }
+})
 
 module.exports = router;
